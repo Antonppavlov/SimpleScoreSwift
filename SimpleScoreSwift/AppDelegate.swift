74d3be62bb7ab307a227aa63635add99fd7c208d
apavlov <a.pavlov@admitad.com>
2017-11-24 17:21:54 +0300

    commit

--- SimpleScoreSwift/AppDelegate.swift
+++ SimpleScoreSwift/AppDelegate.swift
@@ -1,40 +1,46 @@
 //
 //  AppDelegate.swift
-//  SimpleScoreSwift
+//  eqeqw
 //
-//  Created by Anton Pavlov on 23.11.2017.
+//  Created by Anton Pavlov on 24.11.2017.
 //  Copyright © 2017 Anton Pavlov. All rights reserved.
 //
 
 import UIKit
-import AdmitadSDK
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
-    let admitadTracker = AdmitadTracker.sharedInstance
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        admitadTracker.postbackKey = "46913BC31c581bbF4ebAEB4119eb88D4"
-        admitadTracker.trackAppLaunch()
-        admitadTracker.trackReturnedEvent()
-        admitadTracker.trackLoyaltyEvent()
         // Override point for customization after application launch.
         return true
     }
     
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Swift.Void) -> Bool{
-        admitadTracker.continueUserActivity(userActivity)
-        return true
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        admitadTracker.openUrl(url)
-        return true
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
-
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    
 }
 
