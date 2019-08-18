431fafe65056edb310d01b2e799a3382b3b09e30
Antonppavlov <anton.it.pavlov@gmail.com>
2019-08-18 21:37:59 +0300

    убрал сохранение состояния

--- SimpleScoreSwift/AppDelegate.swift
+++ SimpleScoreSwift/AppDelegate.swift
@@ -12,10 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    
+     let storage = AppStorage()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        storage.save(allMoney: 0)
         return true
     }
     
